1f9692180a8c9e59399990730a0c5df7f197c25e
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-08 10:14:24 -0700

    Add: sad path conditional to munchie controller and facade

--- app/controllers/api/v1/munchies_controller.rb
+++ app/controllers/api/v1/munchies_controller.rb
@@ -2,8 +2,11 @@ class Api::V1::MunchiesController < ApplicationController
     def index
         restaurant = MunchieFacade.restaurant_destination(params[:location], params[:food])
         forecast = ForecastFacade.find_forecast(params[:location])
-        
-        render json: MunchieSerializer.munchie_data(restaurant, forecast)
-        
+
+        if !restaurant.nil?
+            render json: MunchieSerializer.munchie_data(restaurant, forecast) 
+        else
+            render json: {data: { message: "Invalid Input" } }, status: 400
+        end
     end
 end
--- app/facades/munchie_facade.rb
+++ app/facades/munchie_facade.rb
@@ -1,6 +1,9 @@
 class MunchieFacade
     def self.restaurant_destination(location, cuisine)
         json = MunchieService.get_restaurant_destination(location, cuisine)
-        restaurant = json[:businesses].first
+
+        if !json[:error]
+            restaurant = json[:businesses].first
+        end
     end
 end
