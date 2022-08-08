508b3e9b3d66c16758fb97b1d8d28bd462284724
Antonio Salmeron <mr.tonyboi@gmail.com>
2022-08-08 08:12:53 -0700

    Refactor: restful routes, add sessions post

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      get 'forecast', to: 'forecast#index'
+      resources :forecast, only: [:index]
 
-      post 'users', to: 'users#create'
+      resources :users, only: [:create]
+
+      resources :sessions, only: [:create]
     end
   end
 end
